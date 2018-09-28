c06bae40a2ca61b80076a82b5e1d9cc9a18ff347
Владислав Мороз <vladislav.moroz103@gmail.com>
2018-09-28 12:26:48 +0300

    a2lab

---
+++ 1.4/first.js
@@ -0,0 +1,17 @@
+function A() {
+        document.getElementById('table').style.display="none";
+
+    }
+
+
+function showTable(){
+   
+    document.getElementById('table2').style.visibility = "hidden";
+     document.getElementById('table1').style.visibility = "visible";
+
+}
+function hideTable(){
+    document.getElementById('table1').style.visibility = "hidden";
+    document.getElementById('table2').style.visibility ="visible";
+
+}
