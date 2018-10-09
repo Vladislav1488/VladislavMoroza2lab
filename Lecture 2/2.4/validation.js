bab1d2c742fe07fae2900a1048ba98de0e57578e
Владислав Мороз <vladislav.moroz103@gmail.com>
2018-10-09 09:19:35 +0300

    a2lab

--- Lecture 2/2.4/validation.js
+++ Lecture 2/2.4/validation.js
@@ -2,7 +2,7 @@ function validateForm() {
     var password1 = document.getElementById('password1');
     var password1_regexp = /(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9!@#$%^&*a-zA-Z]{8,}/g;
     if (!password1_regexp.test(password1.value)) {
-        alert('В пароле должен быть один из сиволов @#$%&, маленькая и большая буквы, цифры')
+        alert('В пароле должен быть один из сиволов @#$%&, маленькая и большая буквы, цифры, не менее 8 символов')
         return false;
     } else if (/^(?=.*\d)(?=.*[.,/'"])/.test(password1.value)) {
         alert('В пароле есть недопустимые символы .,/ ');
