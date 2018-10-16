245c7c04d80b259b2c816d8e72e5981cdf12afdc
Владислав Мороз <vladislav.moroz103@gmail.com>
2018-10-16 13:13:18 +0300

    a2lab

--- Lecture 2/2.1/coder.js
+++ Lecture 2/2.1/coder.js
@@ -1,12 +1,27 @@
-function caesar_code(a, d) {
-    var c = document.getElementById("b_text1"),
-        g = document.getElementById("b_text2"),
-        b = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-        e = "",
-        d = d % b.length || 0,
-        c = c.value.toUpperCase(),
-        f = b.substr(d) + b.substr(0, d);
-    1 == a && (a = b, b = f, f = a);
-    for (i = 0; i < c.length; i++) a = b.lastIndexOf(c.charAt(i)), a = f.charAt(a) || c.charAt(i), e += a;
-    g.value = e
-};
+function doCrypt(isDecrypt) {
+    var shiftText = document.getElementById("shift").value;
+    if (!/^-?\d+$/.test(shiftText)) {
+        alert("Запрещенный символ");
+        return;
+    }
+    var shift = parseInt(shiftText, 10);
+    if (shift < 0 || shift >= 26) {
+        alert("Сдвиг вне допустимого диапазона");
+        return;
+    }
+    if (isDecrypt)
+        shift = (26 - shift) % 26;
+    var textElem = document.getElementById("text");
+    textElem.value = caesarShift(textElem.value, shift);
+}
+
+function caesarShift(text, shift) {
+    var result = "";
+    for (var i = 0; i < text.length; i++) {
+        var c = text.charCodeAt(i);
+        if (65 <= c && c <= 90) result += String.fromCharCode((c - 65 + shift) % 26 + 65);
+        else if (97 <= c && c <= 122) result += String.fromCharCode((c - 97 + shift) % 26 + 97);
+        else result += text.charAt(i);
+    }
+    return result;
+}
