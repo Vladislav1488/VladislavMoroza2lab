b474d6237a01053a7c67ec09d8724195692869e5
Владислав Мороз <vladislav.moroz103@gmail.com>
2018-10-19 11:28:22 +0300

    a2lab

--- Lecture 2/2.2/coder.js
+++ Lecture 2/2.2/coder.js
@@ -1,25 +1,25 @@
 function doCrypt(isDecrypt) {
-    if (document.getElementById("key").value.length == 0) {
+    if (document.getElementById("key").value.length === 0) {
         alert("Key is empty");
         return;
     }
-    var key = filterKey(document.getElementById("key").value);
-    if (key.length == 0) {
+    let key = filterKey(document.getElementById("key").value);
+    if (key.length === 0) {
         alert("Key has no letters");
         return;
     }
     if (isDecrypt) {
-        for (var i = 0; i < key.length; i++)
+        for (let i = 0; i < key.length; i++)
             key[i] = (26 - key[i]) % 26;
     }
-    var textElem = document.getElementById("text");
+    let textElem = document.getElementById("text");
     textElem.value = crypt(textElem.value, key);
 }
 
 function crypt(input, key) {
-    var output = "";
-    for (var i = 0, j = 0; i < input.length; i++) {
-        var c = input.charCodeAt(i);
+    let output = "";
+    for (let i = 0, j = 0; i < input.length; i++) {
+        let c = input.charCodeAt(i);
         if (isUppercase(c)) {
             output += String.fromCharCode((c - 65 + key[j % key.length]) % 26 + 65);
             j++;
@@ -29,17 +29,21 @@ function crypt(input, key) {
         } else {
             output += input.charAt(i);
         }
+        document.getElementById("result").value = output;
     }
-    return output;
+
+return input;
+
 }
 
 function filterKey(key) {
-    var result = [];
-    for (var i = 0; i < key.length; i++) {
-        var c = key.charCodeAt(i);
+    let result = [];
+    for (let i = 0; i < key.length; i++) {
+        let c = key.charCodeAt(i);
         if (isLetter(c))
             result.push((c - 65) % 32);
     }
+
     return result;
 }
 
