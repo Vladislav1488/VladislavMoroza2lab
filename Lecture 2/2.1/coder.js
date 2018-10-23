cdb6ba3bf956d95f97279a42764ee76e9ba7d199
Владислав Мороз <vladislav.moroz103@gmail.com>
2018-10-23 12:36:36 +0300

    a2lab

--- Lecture 2/2.1/coder.js
+++ Lecture 2/2.1/coder.js
@@ -14,7 +14,6 @@ function doCrypt(isDecrypt) {
     var textElem = document.getElementById("text");
     textElem.value = caesarShift(textElem.value, shift);
 }
-
 function caesarShift(text, shift) {
     var result = "";
     for (var i = 0; i < text.length; i++) {
@@ -22,6 +21,8 @@ function caesarShift(text, shift) {
         if (65 <= c && c <= 90) result += String.fromCharCode((c - 65 + shift) % 26 + 65);
         else if (97 <= c && c <= 122) result += String.fromCharCode((c - 97 + shift) % 26 + 97);
         else result += text.charAt(i);
+        document.getElementById('result').value = result;
     }
-    return result;
+   return text;
+
 }
